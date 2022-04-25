024bf4583adfa26c3fd21c9d2c55f5fa0abd4a3f
sadsackk <ilyakukingd@gmail.com>
2022-04-25 19:00:59 +0300

    Добавьте файлы проекта.

---
+++ STP-ISS-analyzer/Models/Position.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STP_ISS_analyzer.Models
+{
+    internal class Position
+    {
+        
+        public List<Transition> transitionsList = new List<Transition>();
+
+        public string Name { get; }
+
+        public Position(string name)
+        {
+            Name = name;
+
+            transitionsList = new List<Transition>();
+        }
+    }
+}
---
+++ STP-ISS-analyzer/Models/Timetable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STP_ISS_analyzer.Models
+{
+    internal class Timetable
+    {
+    }
+}
