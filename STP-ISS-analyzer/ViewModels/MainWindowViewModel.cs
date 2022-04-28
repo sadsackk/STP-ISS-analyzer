27c58dfc67a5a32bf432473d4a07cc8e212eecd3
sadsackk <ilyakukingd@gmail.com>
2022-04-29 00:45:40 +0300

    Начальная Разметка

--- STP-ISS-analyzer/ViewModels/MainWindowViewModel.cs
+++ STP-ISS-analyzer/ViewModels/MainWindowViewModel.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using STP_ISS_analyzer.ViewModels.Base;
 
 namespace STP_ISS_analyzer.ViewModels
 {
-    class MainWindowViewModel
+    class MainWindowViewModel : ViewModel
     {
+        private string _Title = "STP-ISS-Analyzer";
 
+        /// <summary>Заголовок Окна</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
     }
 }
