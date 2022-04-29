4fc9cc9344ab4800d40526b9f9377e68db2df08f
sadsackk <ilyakukingd@gmail.com>
2022-04-29 11:01:19 +0300

    Базовый класс команды

--- STP-ISS-analyzer/ViewModels/MainWindowViewModel.cs
+++ STP-ISS-analyzer/ViewModels/MainWindowViewModel.cs
@@ -17,5 +17,15 @@ namespace STP_ISS_analyzer.ViewModels
             get => _Title;
             set => Set(ref _Title, value);
         }
+
+        private string _Status = "Готов";
+
+        /// <summary>Статус программы</summary>
+        public string Status
+        {
+            get => _Status;
+            set => Set(ref _Status, value);
+        }
     }
 }
+
