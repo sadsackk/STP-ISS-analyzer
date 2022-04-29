59d13c5ca9a34fa9c094ae7caf951b6c1a4dd402
sadsackk <ilyakukingd@gmail.com>
2022-04-29 11:11:30 +0300

    Главный класс команды

---
+++ STP-ISS-analyzer/Infrastructure/Commands/RelayCommand.cs
@@ -0,0 +1,26 @@
+﻿using STP_ISS_analyzer.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STP_ISS_analyzer.Infrastructure.Commands
+{
+    internal class RelayCommand: Command
+    {
+
+        private readonly Action<object> _Execute;
+        public readonly Func<object, bool> _CanExecute;
+
+        public RelayCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Execute(parameter);
+    }
+}
